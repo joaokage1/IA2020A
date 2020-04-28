6dd0f8584be2ae11025aedeea644fc6a642686aa
joaokage1 <j_vgomes@hotmail.com>
2020-04-28 16:57:49 -0300

    Update redeAdaline_professor.py

--- Trabalho 6 - Adaline/redeAdaline_professor.py
+++ Trabalho 6 - Adaline/redeAdaline_professor.py
@@ -12,6 +12,7 @@ entrada = np.loadtxt('entrada.txt') #Falta copiar o arquivo do professor, tentar
 target = np.loadtxt('target.csv', delimiter=';', skiprows=0)#Falta copiar o arquivo do professor 2, tentar enxergar do vídeo rsrs
 (numClasses,targets) = np.shape(target)
 
+#variaveis
 limiar = 0.0
 alfa = 0.01
 erroTolerado = 0.1
@@ -21,6 +22,8 @@ vetor1 = []
 vetor2 = []
 yIn = np.zeros((numClasses,1))
 y = np.zeros((numClasses,1))
+erro = 10
+ciclo = 0
 
 for i in range(entradas):
     for j in range(numClasses):
@@ -29,3 +32,40 @@ for i in range(entradas):
 for j in range(numClasses):
     v0[j] = rd.uniform(-0.1, 0.1)
 
+
+#treinamento rede
+while erro > erroTolerado:
+    ciclo = ciclo + 1
+    erro = 0
+    for i in range(amostras):
+        xaux = entrada[i,:]
+        for m in range(numClasses):
+            soma = 0
+            for n in range(entradas):
+                soma = soma + xaux[n] * v[n][m]
+            yIn[m] = soma + v0[m]
+        for j in range(numClasses):
+            if(yIn[j] >= limiar):
+                y[j] = 1.0
+            else:
+                y[j] = - 1.0
+        for j in range(numClasses):
+            erro = erro + 0.5*((target[j][i] - y[j])**2)
+        
+        vAnterior = v
+
+        for m in range(entradas):
+            for n in range(numClasses):
+                v[m][n] = vAnterior[m][n] + alfa * ((target[n][i]) - y[n]) * xaux[m]
+        
+        v0Anterior = v0
+        for j in range(numClasses):
+            v0[j] = v0Anterior[j] + alfa * ((target[j][i]) - y[j])
+    
+    vetor1.append(ciclo)
+    vetor2.append(erro)
+
+    plt.scatter(vetor1,vetor2, marker='*', color="blue")
+    plt.xlabel('ciclo')
+    plt.ylabel('erro')
+    plt.show()
